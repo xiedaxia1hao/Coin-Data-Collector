bcf744a5c7cb52aeee572e7ff9f49d87daed3603
Ubuntu <ubuntu@ip-172-26-5-165.ec2.internal>
2021-04-13 02:24:08 +0000

    add test

---
+++ test.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from pyvirtualdisplay import Display
+
+
+
+display = Display(visible=0, size=(800, 600))
+display.start()
+
+chrome_options = Options()
+#argument to switch off suid sandBox and no sandBox in Chrome 
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-setuid-sandbox")
+  
+driver = webdriver.Chrome('/usr/bin/chromedriver_linux', chrome_options=chrome_options) 
+driver.get('http://kean.edu')
+print(driver.title)
+driver.quit()
+

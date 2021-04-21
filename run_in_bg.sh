f1d6c5a0039f17ce6d1fb6b426db946585a3ede6
Ubuntu <ubuntu@ip-172-26-14-56.ap-northeast-1.compute.internal>
2021-04-21 18:46:38 +0800

    fix coinwind and sushi web changed

--- run_in_bg.sh
+++ run_in_bg.sh
@@ -1,13 +1,14 @@
 while true; 
 
 do 
-	timeout 300 sh -c "venv/bin/python3 xx.py"; 
+	timeout 500 sh -c "venv/bin/python3 xx.py"; 
 	echo "SLEEPING..."; 
 	sleep 5; 
 	pkill -9 chrome; 
 	pkill -9 python; 
 	pkill -9 Xvfb;
-       	sudo sh -c 'echo 3 >/proc/sys/vm/drop_caches'	
+	sh clear_data.sh;
+       	sudo sh -c 'echo 3 >/proc/sys/vm/drop_caches';
 	echo "PROCESSES KILLED AND MEMORY CLEARED! "; 
 	sleep 5;  
 done
